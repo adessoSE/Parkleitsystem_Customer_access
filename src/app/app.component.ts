fd17f673e2a7af1d6f081ff1f7aba319ccfde805
Melina Lanvermann <melina.lanvermann@adesso.de>
2024-01-23 14:48:54 +0100

    Issue #1

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,15 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import {HeaderComponent} from "./header/header.component";
+import {StartPageComponent} from "./start-page/start-page.component";
+import {BookingPageComponent} from "./booking-page/booking-page.component";
+import {ReservationsPageComponent} from "./reservations-page/reservations-page.component";
+import {FooterComponent} from "./footer/footer.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, HeaderComponent, StartPageComponent, BookingPageComponent, ReservationsPageComponent, FooterComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
---
+++ src/app/reservations-page/reservations-page.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-reservations-page',
+  standalone: true,
+  imports: [],
+  templateUrl: './reservations-page.component.html',
+  styleUrl: './reservations-page.component.css'
+})
+export class ReservationsPageComponent {
+
+}
